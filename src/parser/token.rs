5bcba45cee89f387b3c7b7e2ee84a0206655d9c1
Nipun <shehanatuk@gmail.com>
2022-12-12 21:42:22 -0500

    added more to src/parser/token.rs

---
+++ src/parser/token.rs
@@ -0,0 +1,54 @@
+use self::Token::*;
+
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Token {
+    LPAREN,
+    RPAREN,
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    CARET,
+    EQUALS,
+    NUMBER(f64),
+    SYMBOL(String),
+    EOF
+}
+
+impl Token {
+    /* returns (prec, associativity) where 0 is left and 1 is right  */
+    pub fn info(&self) -> Option<(usize, usize)> {
+        match *self {
+            ADD | SUB => Some((10, 0)),
+            MUL | DIV => Some((20, 0)),
+            CARET => Some((30, 1)),
+            _ => { None }
+        }
+    }
+
+    pub fn to_char(&self) -> char {
+        match *self {
+            LPAREN => '(',
+            RPAREN => ')',
+            ADD => '+',
+            SUB => '-',
+            MUL => '*',
+            DIV => '/',
+            CARET => '^',
+            EQUALS => '=',
+            EOF => 'E',
+            NUMBER(_) => 'N',
+            SYMBOL(_) => 'S',
+        }
+    }
+}
+
+pub fn is_eof(t: &Token) -> bool{
+    match t {
+        &EOF => true,
+        _ => false
+    }
+}
+
+
