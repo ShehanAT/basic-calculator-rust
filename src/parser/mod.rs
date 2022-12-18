b05981520aae853c1a5a7b8c748f37638a2a6dfb
Nipun <shehanatuk@gmail.com>
2022-12-18 17:20:43 -0500

    added more to src/parser/mod.rs, created src/parser/ast.rs

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -67,6 +67,124 @@ impl Parser {
                 try!(self.expect(')'));
                 Ok(e) 
             }
+            NUMBER(val) => {
+                try!(self.next_token());
+                Ok(Box::new(ast::Num { num: val }))
+            }
+            SYMBOL(val) => {
+                // Only allows math functions for now, no variables 
+                try!(self.next_token());
+                match try!(self.peek_token()) {
+                    LPAREN => {
+                        try!(self.expect('('));
+                        let e = try!(self.expr(1));
+                        try!(self.expect(')'));
+                        Ok(self.function(val,e))
+                    }
+                    SYMBOL(name) => {
+                        match &val[..] {
+                            "let" => {
+                                try!(self.next_token());
+                                try!(self.expect('='));
+                                let expr = try!(self.expr(1));
+                                Ok(Box::new( ast::Assignment { name: name, value: expr }))
+                            }
+                            _ => {
+                                Err("Error: two consecutive symbols".to_string())
+                            }
+                        }
+                    }
+                    _ => {
+                        Ok(Box::new( ast::Var { name: val }))
+                    }
+                }
+            }
+            a => {
+                Err(format!("unrecognized atom: {:?}", a))
+            }
         }
+    }
+
+    pub fn op(&self, op: token::Token, lhs: Box<ast::Node>, rhs: Box<ast::Node>) -> Box<ast::Node> {
+        match op {
+            ADD => {
+                Box::new(ast::Add {
+                    left: lhs,
+                    right: rhs,
+                })
+            }
+            SUB => {
+                Box::new( ast::Sub {
+                    left: lhs,
+                    right: rhs,
+                })
+            }
+            MUL => {
+                Box::new( ast::Mul {
+                    left: lhs,
+                    right: rhs,
+                })
+            }
+            DIV => {
+                Box::new( ast::Div {
+                    left: lhs,
+                    right: rhs,
+                })
+            }
+            CARET => {
+                Box::new( ast::Pow {
+                    base: lhs,
+                    exponent: rhs 
+                })
+            }
+            o => {
+                panic!("Unrecognized operation: {:?}", o);
+            }
+        }
+    }
+
+    pub fn function<'a>(&'a self, op: String, arg: Box<ast::Node>) -> Box<ast::Node> {
+        match &op[..] {
+            "sin" | "sine" => {
+                Box::new( ast::Sin {
+                    arg: arg
+                })
+            }
+            "sqrt | SQRT" => {
+                // Box::new( ast::sqrt)
+            }
+        }
+    }
+
+}
+
+impl Parser {
+    pub fn expect(&mut self, tok: char) -> Result<(), String> {
+        try!(self.next_token());
+        if self.current.to_char() != tok {
+            return Err(format!("expected {:} but found {}", tok, self.current_to_char()));
+        }
+        Ok(())
+    }
+
+    pub fn peek_token(&mut self) -> Result<token::Token, String> {
+        if self.peeked.is_none() {
+            self.peeked = Some(try!(self.lexer.next_token()));
+        }
+
+        Ok(self.peeked.clone().unwrap())
+    }
+
+    pub fn next_token(&mut self) -> Result<(), String> {
+        match self.peeked {
+            Some(ref mut pk) => {
+                self.current = pk.clone();
+            }
+            None => {
+                self.current = try!(self.lexer.next_token());
+            }
+        }
+        self.peeked = None;
+        Ok(())
     }
 }
